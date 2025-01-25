999106b5a166141f05a5af2b697fe03cbcf14797
Shpak Kuzma <65441450+R3pzz@users.noreply.github.com>
2025-01-25 13:08:04 +0000

    Changed the .gitignore file.

---
+++ src/idafucker-engine/idafucker-engine.cpp
@@ -0,0 +1,12 @@
+﻿// idafucker-engine.cpp : Defines the entry point for the application.
+//
+
+#include "idafucker-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
