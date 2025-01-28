cd0c162344ad901265cc0d196f2f873926897d03
vmprotector <)))))))))))))))))>
2025-01-28 17:26:48 +0300

    Formatted namings + dir style

---
+++ idafucker/src/idafucker.cpp
@@ -0,0 +1,7 @@
+#include <idafucker/CoreDefines.hpp>
+
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow) {
+	// delegate to real here
+}
