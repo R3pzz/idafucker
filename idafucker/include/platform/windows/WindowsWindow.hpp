c155e7fd921e07b461a30b9438b9443e84513aec
Shpak Kuzma <65441450+R3pzz@users.noreply.github.com>
2025-01-28 14:19:21 +0000

    Implemented application core, updated the package manifest.

---
+++ idafucker/include/platform/windows/WindowsWindow.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <CoreDefines.hpp>
+
+#include <string> // wstring
+
+IDAFUCKER_NAMESPACE_BEGIN
+
+class WindowsAppWindow {
+public:
+  struct CreateParams {
+    DWORD style{};
+    DWORD styleEx{};
+    RECT rect{};
+    std::wstring title{};
+    bool customTitleBar{};
+
+    [[nodiscard]] constexpr int width() const noexcept { return rect.right - rect.left; }
+    [[nodiscard]] constexpr int height() const noexcept { return rect.bottom - rect.top; }
+  };
+
+  WindowsAppWindow(const CreateParams &params, LPCWSTR className);
+  constexpr WindowsAppWindow(const WindowsAppWindow &) noexcept = default;
+
+  ~WindowsAppWindow();
+
+  void setVisibility(bool visible);
+
+  [[nodiscard]] constexpr HWND hwnd() const noexcept { return _hwnd; }
+
+  static LRESULT WINAPI wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+  HWND _hwnd{};
+};
+
+IDAFUCKER_NAMESPACE_END
--- idafucker/src/EntryPoint.cpp
+++ idafucker/src/EntryPoint.cpp
@@ -1,3 +1,28 @@
 #pragma once
-#include <CoreDefines.hpp>
+#include <platform/Application.hpp>
+#include <platform/Window.hpp>
 
+#include <print> // print
+
+int main(int argc, char* argv[]) {
+  try {
+    idafucker::App app{argc, argv};
+    auto &mainWnd = app.createWindow(
+        {.style{WS_OVERLAPPEDWINDOW}, .rect{0, 0, 1920, 1080}, .title{L"idafucker"}, .customTitleBar{true}});
+    mainWnd.setVisibility(true);
+
+    while (app.running()) {
+      MSG msg{};
+
+      while (::PeekMessage(&msg, nullptr, 0u, 0u, PM_REMOVE)) {
+        ::TranslateMessage(&msg);
+        ::DispatchMessage(&msg);
+      }
+    }
+  } catch (std::exception &e) {
+    std::print("Exception caught: {}\n", e.what());
+    std::this_thread::sleep_for(std::chrono::seconds{10u});
+  }
+
+  return 0;
+}
